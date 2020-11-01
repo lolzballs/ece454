9c15758729ed1f83a85f6f504201c0a8243bb659
Benjamin Cheng <ben@bcheng.me>
2020-10-31 21:23:19 -0600

    Remove some of the original code

--- ece454-lab3/src/mm.c
+++ ece454-lab3/src/mm.c
@@ -91,47 +91,6 @@ uint8_t *heap_top;
  }
 
 /**********************************************************
- * coalesce
- * Covers the 4 cases discussed in the text:
- * - both neighbours are allocated
- * - the next block is available for coalescing
- * - the previous block is available for coalescing
- * - both neighbours are available for coalescing
- **********************************************************/
-void *coalesce(void *bp)
-{
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    size_t size = GET_SIZE(HDRP(bp));
-
-    if (prev_alloc && next_alloc) {       /* Case 1 */
-        return bp;
-    }
-
-    else if (prev_alloc && !next_alloc) { /* Case 2 */
-        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-        PUT(HDRP(bp), PACK(size, 0));
-        PUT(FTRP(bp), PACK(size, 0));
-        return (bp);
-    }
-
-    else if (!prev_alloc && next_alloc) { /* Case 3 */
-        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-        PUT(FTRP(bp), PACK(size, 0));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        return (PREV_BLKP(bp));
-    }
-
-    else {            /* Case 4 */
-        size += GET_SIZE(HDRP(PREV_BLKP(bp)))  +
-            GET_SIZE(FTRP(NEXT_BLKP(bp)))  ;
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size,0));
-        PUT(FTRP(NEXT_BLKP(bp)), PACK(size,0));
-        return (PREV_BLKP(bp));
-    }
-}
-
-/**********************************************************
  * extend_heap
  * Extend the heap by "words" words, maintaining alignment
  * requirements of course. Free the former epilogue block
@@ -254,8 +213,6 @@ void mm_free(void *bp) {
     if (bp == NULL) {
         return;
     }
-
-    assert(mm_check());
 
     uint64_t size = GET_SIZE(HDRP(bp));
     // check next contiguous block for coalescing
